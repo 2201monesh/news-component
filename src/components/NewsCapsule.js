c9e123a15f723e38d287de8bf13de17dd6ebd322
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-16 20:24:51 +0530

    Added the UI changes

--- src/components/NewsCapsule.js
+++ src/components/NewsCapsule.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 function NewsCapsule(props) {
 
-    const maxLength = 15;
+    const maxLength = 17;
 
   return (
     <div className='news-capsule-div'>
-     {props.news.map((item) => <p className='news-capsule'>{item.title.substr(0, maxLength)}</p> )}
+     {/* {props.news.map((item) => <p className='news-capsule'>{item.title.substr(0, maxLength)}</p> )} */}
+     {props.news.map((item) => (
+         <p className='news-capsule'>
+           <a href={item.url}>{item.title.substr(0, maxLength)}</a>
+         </p> 
+         ))}
     </div>
   )
 }
