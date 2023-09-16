d093aeb4ea44ad7e29064d031b4361693710b9d4
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-16 14:56:07 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainCard from './components/MainCard'
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainCard() {
+  return (
+    <div>
+      <span>icon</span>
+      <p>Trending searches</p>
+    </div>
+  )
+}
+
+export default MainCard;
